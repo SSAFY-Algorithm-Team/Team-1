a68d86718f376846e54d988cf85bd38b67725025
serim <serim38@gmail.com>
2026-08-27 12:02:44 +0900

    fix: [coders 1732] 반복횟수 간선개수(m)으로 수정, 내림차순 정렬

--- serim/week-03/coders_1732_깊이우선탐색(DFS).py
+++ serim/week-03/coders_1732_깊이우선탐색(DFS).py
@@ -7,7 +7,7 @@
 # 다음 m개의 줄에 간선의 정보가 주어짐- 간선이 연결하는 두점 번호
 
 # [출력]
-# 탐색을 시작한 정점부터 차례로 반문한 정점 순서대로 출력
+# 탐색을 시작한 정점부터 차례로 방문한 정점 순서대로 출력
 
 # 정점 개수, 간선 개수, 탐색시작정점 번호
 n, m, v = map(int, input().split())
@@ -17,7 +17,7 @@ n, m, v = map(int, input().split())
 # 인접리스트 먼저 구현
 adj_list = [[] for _ in range(n+1)]
 
-for _ in range(n+1):
+for _ in range(m):  # 정점이 아니라 간선 개수만큼 반복
     start, end = map(int, input().split())
     adj_list[start].append(end)
     adj_list[end].append(start)
@@ -32,6 +32,7 @@ while stack:
         for des in adj_list[current]:
             if des not in visited:
                 stack.append(des)
-        stack.sort()
+        stack.sort(reverse=True)    # 내림차순으로 정렬
+        # 내림차 순으로 정렬해야 작은 노드부터 탐색할 수 있음
 
 print(*visited)
